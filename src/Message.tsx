130f7023489d19b452cef702eefddf363a4d1d01
alexzhangryan <alexzhangryan@gmail.com>
2025-09-09 14:33:42 -0500

    fixed some sizing issues

--- src/Message.tsx
+++ src/Message.tsx
@@ -7,7 +7,7 @@ const Message = () => {
         <>  
             <Typewriter />
             <p className="text-center w-1/2 m-auto">Currently a student at The Univeristy of Wisconsin - Madison pursuing majors in Computer Science and Economics with a Mathematical Emphasis. I have experience in languages such as Python, Java, TypeScript, and SQL as well as libraries such as React, Pytorch, and SQLAlchemy.</p>
-            <div className="grid grid-cols-4 gap-4 justify-center my-5 max-w-1/6 mx-auto">
+            <div className="grid grid-cols-4 gap-4 justify-center my-5 w-75 mx-auto">
                 <a href="https://github.com/alexzhangryan"><img className = "linkImage" src="./src/assets/github.png"></img></a>
                 <a href="https://www.linkedin.com/in/alex-ryan-807b22293/"><img className = "linkImage" src="./src/assets/linkedin.png"></img></a>
                 <a href="mailto:alexzhangryan@gmail.com"><img className = "linkImage" src="./src/assets/gmail.jpg"></img></a>
--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -13,7 +13,7 @@ export default function ProjectCard(props: ProjectCardProps){
     return(
         <div className="projectCard" style={{ animationDelay: `${props.index / 6}s`}}>
             <img className="h-50 rounded-l-4xl"src={`./src/assets/${props.img}`}></img>
-            <div className="grid grid-rows-4 col-span-3">
+            <div className="grid grid-rows-4 col-span-3 px-2">
                 <h1 className="text-[24px] font-bold">{props.title}</h1>
                 <p className="text-[16px] row-span-2">{props.desc}</p>
                 <ul className="flex items-center">
